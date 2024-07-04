a406674f4986f30aa1722c35676b5c9bbf38414e
ThomasB <t.blasi27@gmail.com>
2024-07-04 14:51:31 +0200

    implementation of storage class + minio storage class

--- main/connectors/storage_connectors.py
+++ main/connectors/storage_connectors.py
@@ -28,23 +28,35 @@ class MinIOStorage(StorageInterface):
         else:
             print('Bucket', bucket_name, 'found')
         
-    def upload(self, source, bucket_name, destination):
+    def upload(self, source_file, bucket_name, destination):
         self.helper_check_bucket(self, bucket_name)
-        self.client.fput_object(bucket_name, destination, source)
+        self.client.fput_object(bucket_name, destination, source_file)
         
         print(
-            source, "successfully uploaded as object", 
-            destination, "to bucket", bucket_name,
+            source_file, "successfully uploaded as object", 
+            "to bucket", bucket_name, "and path", destination
         )
 
-    def download(self, source, bucket_name, destination):
+    def download(self, source_file, bucket_name, destination):
         self.helper_check_bucket(self, bucket_name)
-        self.client.fget_object(bucket_name, destination, source)
+        self.client.fget_object(bucket_name, destination, source_file)
         
         print(
-            source, "successfully downloaded as object", 
-            destination, "to bucket", bucket_name,
+            source_file, "successfully downloaded as object", 
+            "to bucket", bucket_name, "and path", destination
         )
+        
+    def list_files(self, bucket_name, path):
+        self.helper_check_bucket(self, bucket_name)
+        objects = self.client.list_objects(bucket_name, suffix = path)
+        return [obj.object_name for obj in objects]
+
+
+    def delete(self, bucket_name, file_path):
+        self.client.remove_object(bucket_name, file_path)
+        
+        print(file_path, "successfully deleted from bucket", bucket_name)
+        
         
         
 #TODO: create class DuckDBStorage(StorageInterface) once to finish dagster worfklows for some transformations
